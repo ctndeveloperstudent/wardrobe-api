53ec2ee9df7a4a183df89e11db7ae4b4105e0c29
ctndeveloperstudent <15708@coderacademy.edu.au>
2024-09-28 17:25:19 +1000

    connected flask to postgreSQL

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+# Create the Flask app instance
 app = Flask(__name__)
 
+# Configure the database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:@localhost:5432/wardrobe_db'
+
+# Initialize the SQLAlchemy object
+db = SQLAlchemy(app)
+
 @app.route('/')
-def home ():
-    return "Wardrobe API is running!"
+def home():
+    return "Wardrobe API connected to DB!"
 
 if __name__ == '__main__':
     app.run(debug=True)
